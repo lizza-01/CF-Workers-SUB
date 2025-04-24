a583f93cb9033896ee430297e0239a8549c56c3f
lizza-01 <lizzaland@outlook.com>
2025-04-24 11:57:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,13 +13,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
-https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt
-https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt
-https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2
-https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/airport_sub_merge.txt
-https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge.txt
-https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
+https://liangxin.xyz/api/v1/liangxin?OwO=7a864947dac8ab875646d3c55cd9212a
 `
 
 let urls = [];
